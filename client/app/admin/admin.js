f78e1728171db9eadf97986c17cc67664b8bfd10
Benjamin Conant <conantbenjamin@gmail.com>
2015-03-20 17:47:59 -0400

    first commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('snapmapApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
