aa5fc4d4716a2e8c35c171f50775843881f16356
Benjamin Conant <conantbenjamin@gmail.com>
2015-03-20 19:41:03 -0400

    User Model done

---
+++ server/api/store/store.controller.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var _ = require('lodash');
+var Store = require('./store.model');
+
+// Get list of stores
+exports.index = function(req, res) {
+  Store.find(function (err, stores) {
+    if(err) { return handleError(res, err); }
+    return res.json(200, stores);
+  });
+};
+
+// Get a single store
+exports.show = function(req, res) {
+  Store.findById(req.params.id, function (err, store) {
+    if(err) { return handleError(res, err); }
+    if(!store) { return res.send(404); }
+    return res.json(store);
+  });
+};
+
+// Creates a new store in the DB.
+exports.create = function(req, res) {
+  Store.create(req.body, function(err, store) {
+    if(err) { return handleError(res, err); }
+    return res.json(201, store);
+  });
+};
+
+// Updates an existing store in the DB.
+exports.update = function(req, res) {
+  if(req.body._id) { delete req.body._id; }
+  Store.findById(req.params.id, function (err, store) {
+    if (err) { return handleError(res, err); }
+    if(!store) { return res.send(404); }
+    var updated = _.merge(store, req.body);
+    updated.save(function (err) {
+      if (err) { return handleError(res, err); }
+      return res.json(200, store);
+    });
+  });
+};
+
+// Deletes a store from the DB.
+exports.destroy = function(req, res) {
+  Store.findById(req.params.id, function (err, store) {
+    if(err) { return handleError(res, err); }
+    if(!store) { return res.send(404); }
+    store.remove(function(err) {
+      if(err) { return handleError(res, err); }
+      return res.send(204);
+    });
+  });
+};
+
+function handleError(res, err) {
+  return res.send(500, err);
+}
