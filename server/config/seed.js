a940f806ee71b3cda52c3fa54b5a01c973681019
Benjamin Conant <conantbenjamin@gmail.com>
2015-03-21 19:15:14 -0400

    seeding

--- server/config/seed.js
+++ server/config/seed.js
@@ -13,6 +13,7 @@ var csv = require('csv');
 var Promise = require('bluebird'); 
 var Foursquare = require('../api/foursquare/foursquare.model'); 
 var request = require('request');
+var _ = require('lodash');
 var config = {
   'secrets': {
    'clientId' : 'Z51AIBECHYHKUHK2R03STTEPGYQDCCI5POSPWURJL1JBVEQC',
@@ -50,111 +51,224 @@ var config = {
 // 'https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&intent=browse&ne=40.827292,-73.935042&sw=40.809104,-73.965598&limit=200&v=20150321&oauth_token=ASYR2H1AHNFDD2TUHKJXCPUOKQ3SFKNTZUGOBQCAMBATHGA5'
 
 
-request('https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&intent=browse&ne=40.827292,-73.935042&sw=40.809104,-73.965598&limit=200&v=20150321&oauth_token=ASYR2H1AHNFDD2TUHKJXCPUOKQ3SFKNTZUGOBQCAMBATHGA5', function(error, response, body){
+// request('https://api.foursquare.com/v2/venues/search?categoryId=4d4b7105d754a06374d81259&intent=browse&ne=40.827292,-73.935042&sw=40.809104,-73.965598&limit=200&v=20150321&oauth_token=ASYR2H1AHNFDD2TUHKJXCPUOKQ3SFKNTZUGOBQCAMBATHGA5', function(error, response, body){
   
-  body = JSON.parse(body);
-  console.log(body);
-    if (error) console.log('error from request: ', error); 
-    console.log('body: ', body);
-    var data = body.response.venues; 
-    data.forEach(function(store, index){
-    if (store){
-      if (!store.delivery) {
-        store.delivery = {
-          provider: {
-            name: null,
-            url: null
-          } 
-        }
-      }
-      Foursquare.create({
-        name: store.name, 
-        storeId: store.id, 
-        storeUrl: store.url,
-        categories: [{
-          name: store.categories[0].name, 
-          id: store.categories[0].id, 
-          icon: {
-            prefix: store.categories[0].icon.prefix, 
-            suffix: store.categories[0].icon.suffix
-          }
-        }], 
-        contactInfo: {
-          phone: store.contact.phone, 
-          formattedPhone: store.contact.formattedStore, 
-          address: store.location.address, 
-          crossStreet: store.location.crossStreet, 
-          formattedAddress: store.location.formattedAddress, 
-          lat: store.location.lat, 
-          lng: store.location.lng, 
-          postalCode: store.location.postalCode
-        }, 
-        delivery: {
-          name: store.delivery.provider.name, 
-          url: store.delivery.provider.url
-        }
-      })
-      .then(function success(successdata){
-        console.log('store: ', successdata)
-      }, function failed(error){
-        console.log('err during creation: ', error)
-      })
-    }
-  })
-});
+//   body = JSON.parse(body);
+//   console.log(body);
+//     if (error) console.log('error from request: ', error); 
+//     console.log('body: ', body);
+//     var data = body.response.venues; 
+//     data.forEach(function(store, index){
+//     if (store){
+//       if (!store.delivery) {
+//         store.delivery = {
+//           provider: {
+//             name: null,
+//             url: null
+//           } 
+//         }
+//       }
+//       Foursquare.create({
+//         name: store.name, 
+//         storeId: store.id, 
+//         storeUrl: store.url,
+//         categories: [{
+//           name: store.categories[0].name, 
+//           id: store.categories[0].id, 
+//           icon: {
+//             prefix: store.categories[0].icon.prefix, 
+//             suffix: store.categories[0].icon.suffix
+//           }
+//         }], 
+//         contactInfo: {
+//           phone: store.contact.phone, 
+//           formattedPhone: store.contact.formattedStore, 
+//           address: store.location.address, 
+//           crossStreet: store.location.crossStreet, 
+//           formattedAddress: store.location.formattedAddress, 
+//           lat: store.location.lat, 
+//           lng: store.location.lng, 
+//           postalCode: store.location.postalCode
+//         }, 
+//         delivery: {
+//           name: store.delivery.provider.name, 
+//           url: store.delivery.provider.url
+//         }
+//       })
+//       .then(function success(successdata){
+//         console.log('store: ', successdata)
+//       }, function failed(error){
+//         console.log('err during creation: ', error)
+//       })
+//     }
+//   })
+// });
 
-// request('https://api.foursquare.com/v2/venues/search?ll=40.7,-74&oauth_token=ASYR2H1AHNFDD2TUHKJXCPUOKQ3SFKNTZUGOBQCAMBATHGA5&v=20150321',  function (error, response, body) {
-//   console.log("Hello", error);
-//   // console.log("hello", response);
-//   console.log("Hello body", body);
-//   if (!error && response.statusCode == 200) {
+// // request('https://api.foursquare.com/v2/venues/search?ll=40.7,-74&oauth_token=ASYR2H1AHNFDD2TUHKJXCPUOKQ3SFKNTZUGOBQCAMBATHGA5&v=20150321',  function (error, response, body) {
+// //   console.log("Hello", error);
+// //   // console.log("hello", response);
+// //   console.log("Hello body", body);
+// //   if (!error && response.statusCode == 200) {
     
-//     console.log('BODY!!!!: ', body) // Show the HTML for the Google homepage.
-//   }
+// //     console.log('BODY!!!!: ', body) // Show the HTML for the Google homepage.
+// //   }
+// // })
+
+
+
+// var foursquare = require('node-foursquare')
+
+
+
+// Store.find({}).remove(function(){
+//   fs.readFile(__dirname+"/../nydata.csv", function(err, data){
+//     // console.log(err);
+//     var storeString = data.toString();
+//     csv.parse(storeString, function(err, stores){
+//         // console.log(err);
+//       stores.forEach(function(store, index) { 
+//         if (index !== 0 && store[5] === "New York") {
+//           // console.log(index + " thing");
+//           Store.create({
+//               name: store[0],
+//               address: store[3],
+//               addressLineTwo: store[4],
+//               lon: store[1],
+//               lat: store[2],
+//               zip5: store[7],
+//               zip4: store[8],
+//               county: store[9],
+//               state: store[6],
+//               city: store[5]
+//           }, function(err, data){
+//             // console.log("fdsafdsafdsaf");
+//             // console.log(err);
+//             // console.log(data);
+//           });
+
+//         } else {
+//           // console.log("not creating " + index);
+//         }
+//       });
+//     });
+//   });
 // })
 
+var storeObjs = Store.find().exec(); 
+var fourObjs = Foursquare.find().exec(); 
 
+function similar_text(first, second, percent) {
+  //  discuss at: http://phpjs.org/functions/similar_text/
+  // original by: Rafał Kukawski (http://blog.kukawski.pl)
+  // bugfixed by: Chris McMacken
+  // bugfixed by: Jarkko Rantavuori original by findings in stackoverflow (http://stackoverflow.com/questions/14136349/how-does-similar-text-work)
+  // improved by: Markus Padourek (taken from http://www.kevinhq.com/2012/06/php-similartext-function-in-javascript_16.html)
+  //   example 1: similar_text('Hello World!', 'Hello phpjs!');
+  //   returns 1: 7
+  //   example 2: similar_text('Hello World!', null);
+  //   returns 2: 0
 
-var foursquare = require('node-foursquare')
+  if (first === null || second === null || typeof first === 'undefined' || typeof second === 'undefined') {
+    return 0;
+  }
 
+  first += '';
+  second += '';
 
+  var pos1 = 0,
+    pos2 = 0,
+    max = 0,
+    firstLength = first.length,
+    secondLength = second.length,
+    p, q, l, sum;
 
-Store.find({}).remove(function(){
-  fs.readFile(__dirname+"/../nydata.csv", function(err, data){
-    // console.log(err);
-    var storeString = data.toString();
-    csv.parse(storeString, function(err, stores){
-        // console.log(err);
-      stores.forEach(function(store, index) { 
-        if (index !== 0 && store[5] === "New York") {
-          // console.log(index + " thing");
-          Store.create({
-              name: store[0],
-              address: store[3],
-              addressLineTwo: store[4],
-              lon: store[1],
-              lat: store[2],
-              zip5: store[7],
-              zip4: store[8],
-              county: store[9],
-              state: store[6],
-              city: store[5]
-          }, function(err, data){
-            // console.log("fdsafdsafdsaf");
-            // console.log(err);
-            // console.log(data);
-          });
+  max = 0;
 
-        } else {
-          // console.log("not creating " + index);
-        }
-      });
-    });
-  });
-})
+  for (p = 0; p < firstLength; p++) {
+    for (q = 0; q < secondLength; q++) {
+      for (l = 0;
+        (p + l < firstLength) && (q + l < secondLength) && (first.charAt(p + l) === second.charAt(q + l)); l++)
+      ;
+      if (l > max) {
+        max = l;
+        pos1 = p;
+        pos2 = q;
+      }
+    }
+  }
 
+  sum = max;
 
+  if (sum) {
+    if (pos1 && pos2) {
+      sum += similar_text(first.substr(0, pos1), second.substr(0, pos2));
+    }
 
+    if ((pos1 + max < firstLength) && (pos2 + max < secondLength)) {
+      sum += similar_text(first.substr(pos1 + max, firstLength - pos1 - max), second.substr(pos2 + max,
+        secondLength - pos2 - max));
+    }
+  }
+
+  if (!percent) {
+    return sum;
+  } else {
+    return (sum * 200) / (firstLength + secondLength);
+  }
+}
+
+var data {
+
+}
+
+// storeObjs.then(function(resolvedStoreObjs){
+//   fourObjs.then(function(resolvedFourObjs){
+//     return resolvedStoreObjs.map(function(store, index){
+//       for (var i = 0; i <= resolvedFourObjs.length; i++){
+//        if (store && resolvedFourObjs[i]){
+//          var bool = similar_text(store.name, resolvedFourObjs[i].name, true)
+//          if (bool){
+//             console.log('in if!!!!!!!!!!: ', store)
+//             var data = {
+//               storeId: resolvedFourObjs[i].storeId,
+//               categories: resolvedFourObjs[i].categories, 
+//               phoneNumber: resolvedFourObjs[i].phone, 
+//               formattedPhone: resolvedFourObjs[i].formattedPhone, 
+//               delivery: resolvedFourObjs[i].delivery, 
+//               storeUrl: resolvedFourObjs[i].storeUrl
+//             };
+//             var updated = _.merge(store, data)
+//             console.log('store2: ', updated)
+//             store.save(function(err, saved){
+//               return new Promise(function(resolve, reject){
+//                 if (err) return reject(err)
+//                 resolve(saved); 
+//               })  
+//             })
+//           } // end of if
+//           else {
+//             // console.log('in else: ', store)
+//             return new Promise(function(resolve, reject){
+//               store.save(function(err, saved, num){
+//                 console.log('num: ', num)
+//                 if (err) return reject(err)
+//                 resolve(saved); 
+//               }) 
+//             })
+//           }   //end of else      
+//         }
+//       }   //end of for loop 
+//     })  
+//   }).then(function success(arrayOfPromises){
+//     return Promise.all(arrayOfPromises); 
+//   }, function failed(error){
+//     console.log('error:', error)
+//   }).then(function success(resolvedNewStores){
+//       console.log('resolved: ', resolvedNewStores.length)
+//   }, function failed(error){
+//     console.log('error: ', error)
+//   })
+// })
 
 
 
