d9f6975a9e2bfa4c09444ee1b7a32805baf95034
Benjamin Conant <conantbenjamin@gmail.com>
2015-03-28 12:14:37 -0400

    pins now load as user moves around the map

--- client/app/map/map.directive.js
+++ client/app/map/map.directive.js
@@ -6,11 +6,28 @@ angular.module('snapmapApp')
       templateUrl: 'app/map/map.html',
       restrict: 'EA',
       link: function (scope, element, attrs) {
-        scope.mapChanged = {dragend: function () {
+        scope.mapChanged = {idle: function () {
              console.log("FOOBAT");
              console.log(scope.map.center);
-            }
-          }
+              data = {
+                coords: [scope.map.center.longitude, scope.map.center.latitude], 
+                dist: 10
+              }
+             store.someMethod(data)
+              .then(function (location){
+                scope.locations = [];
+                scope.groceries = [];
+                console.log('location: ', location)
+                location.forEach(function(store){
+                  scope.locations.push(store);
+                  // if (regex.test(store.name.toLowerCase())) scope.groceries.push(store);
+                  // else scope.locations.push(store);
+                })
+              })
+             }
+           }
+
+            
       	var data; 
       	// var deferred = $q.defer()
       	GeolocationFactory.getGeo().then(function (){
