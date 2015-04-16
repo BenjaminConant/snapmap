4fdf3817bd579a1f1ab7c633b4c58d5eb1d34cbd
Ayana <ayana.d.i.wilson@gmail.com>
2015-04-15 21:07:55 -0400

    modified script for api call making

--- server/config/seed.js
+++ server/config/seed.js
@@ -16,102 +16,18 @@ var Foursquare = require('../api/foursquare/foursquare.model');
 var request = require('request');
 var Promise = require('bluebird'); 
 var requestP = Promise.promisify(require('request')); 
+var StoreGoogle = require('../api/StoreGoogle/StoreGoogle.model')
+var Worker = require('webworker-threads').Worker
 
-function dbSeedString(collection) {
-  return "mongoexport --db snapmap-dev --collection " + collection + " --out " + path.join(__dirname,"/db_data") + "/" + collection + ".json"
-}
+
+// function dbSeedString(collection) {
+//   return "mongoexport --db snapmap-dev --collection " + collection + " --out " + path.join(__dirname,"/db_data") + "/" + collection + ".json"
+// }
 
 
 //exports the store collection into a json file 
 // exec("mongoexport --db snapmap-dev --collection stores --out stores.json")
 
-// Store.find({}).remove(function(){
-//  fs.readFile(__dirname+"/../stores.json", function(err, data){
-//     if (err){
-//       console.log('err: ', err)
-//       throw err; 
-//     }
-//     // var storeString = data.toString();
-//     JSON.parse(data, function(err, stores){
-//         // console.log(err);
-
-//         /*
-//           this is where we will make our api calls !!!
-//         */
-//       stores.forEach(function(store, index) { 
-//         if (index !== 0 && store[5] === "New York") {
-//           // console.log(index + " thing");
-//           Store.create({
-//               name: store[0],
-//               address: store[3],
-//               addressLineTwo: store[4],
-//               lon: store[1],
-//               lat: store[2],
-//               zip5: store[7],
-//               zip4: store[8],
-//               county: store[9],
-//               state: store[6],
-//               city: store[5]
-//           }, function(err, data){
-//             // console.log("fdsafdsafdsaf");
-//             // console.log(err);
-//             // console.log(data);
-//           });
-
-//         } else {
-//           // console.log("not creating " + index);
-//         }
-//       });
-//     });
-//   });
-// })
-
-// //////////////////////////////////////// LOAD DATA IN DB ////////////////////////////////////////////////////////
-
-// resArray.forEach(function(store, index){
-//   if (store){
-//     if (!store.delivery) {
-//       store.delivery = {
-//         provider: {
-//           name: null,
-//           url: null
-//         }
-//       }
-//     }
-//     Foursquare.create({
-//       name: store.name, 
-//       storeId: store.id, 
-//       storeUrl: store.url,
-//       categories: [{
-//         name: store.categories[0].name, 
-//         id: store.categories[0].id, 
-//         icon: {
-//           prefix: store.categories[0].icon.prefix, 
-//           suffix: store.categories[0].icon.suffix
-//         }
-//       }], 
-//       contactInfo: {
-//         phone: store.contact.phone, 
-//         formattedPhone: store.contact.formattedStore, 
-//         address: store.location.address, 
-//         crossStreet: store.location.crossStreet, 
-//         formattedAddress: store.location.formattedAddress, 
-//         lat: store.location.lat, 
-//         lng: store.location.lng, 
-//         postalCode: store.location.postalCode
-//       }, 
-//       delivery: {
-//         name: store.delivery.provider.name, 
-//         url: store.delivery.provider.url
-//       }
-//     })
-//     .then(function success(successdata){
-//       console.log('store: ', successdata)
-//     }, function failed(error){
-//       console.log('err during creation: ', error)
-//     })
-//   } //end of if condition
-// }); 
 
 
 ///////////////////////////////// LOAD GOVERNMENT DATA IN DB //////////////////////////////////////////////////////////
@@ -160,31 +76,25 @@ function dbSeedString(collection) {
 //   })
 // }
 
-///////////////////////////////// LOAD GOVERNMENT DATA IN DB //////////////////////////////////////////////////////////
+///////////////////////////////// PING GOOGLE PLACES TEXT SEARCH API //////////////////////////////////////////////////////////
 
+// var readFile = Promise.promisify(require('fs').readFile);
 
-
-var Store = require('../api/store/store.model'); 
-var Promise = require('bluebird'); 
-var pRequest = require('promisified-request'); 
-var StoreGoogle = require('../api/StoreGoogle/StoreGoogle.model')
-var requestP = Promise.promisify(request)
-
-
-// var Worker = require('webworker-threads').Worker
-//var request = require('request'); 
-//Promise.promisifyAll(request)
-// StoreGoogle.find({}).remove().exec()
-// .then(function(){
-//   Store.find({}).exec()
-//   .then(function(storeObjs){
-//     console.log('in then', storeObjs[0])
-//     storeObjs = storeObjs.slice(0, 50)
-//     storeObjs.forEach(makeApiCallToPlaceSearch)
+// readFile(__dirname + '/../../stores.json')
+// .then(function fulfilled (stores){
+//   //shouldn't be necessary
+//   //data = JSON.parse(data)
+//   return data
+// })
+// .then (function (arrayOfStores){                //.each(makeApiCallToPlaceSearch)
+//   return Promise.map(arrayofStores, function(store){
+//     return makeApiCallToPlaceSearch(store)
 //   })
+// }).then(function (resolved){
+//   // export the transformed objects so we can double check seriality
+//   exec("mongoexport --db snapmap-dev --collection stores --out stores.json")
 // })
 
-// //'https://maps.googleapis.com/maps/api/place/textsearch/json?location=' + place.location[1]+','+ place.location[0] + '&radius=1&sensor=true' +'&query=' + store.name + '&key=AIzaSyDl2B0kbv7OI2PqqvwX6vMboXK9d333G_k'
 
 // function makeApiCallToPlaceSearch(store){
 
@@ -209,28 +119,12 @@ var requestP = Promise.promisify(request)
 //   }) 
 // }
 
-//textsearch
-// 'https://maps.googleapis.com/maps/api/place/textsearch/json?location=' + place.location[1]+','+ place.location[0] + '&radius=1&sensor=true' +'&query=' + store.name + '&key=AIzaSyDl2B0kbv7OI2PqqvwX6vMboXK9d333G_k'
 
-
-// //nearbysearch
-// 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + place.lat+','+ place.lon + '&radius=' 10 + '&sensor=true' + 'query=' + store.name + '&key=AddYourOwnKeyHere'
-
-
-// //most precise queries: 
-// 'https://maps.googleapis.com/maps/api/place/radarsearch/json?location=' + place.lat + ',' + place.lon + '&radius=10&types=food&key=AddYourOwnKeyHere'
-
-// //example of precise query: 
-// 'https://maps.googleapis.com/maps/api/place/radarsearch/json?location=48.859294,2.347589&radius=5000&types=food|cafe&keyword=vegetarian&key=AddYourOwnKeyHere'
-
-
-
-// apikey: AIzaSyDl2B0kbv7OI2PqqvwX6vMboXK9d333G_k
 
 
 /////////////////////////////////////// PLACE DETAILS //////////////////////////////////////////////////////////////////////////////
 
-    // var urlPlaceDetails = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' +placeId + '&key=' + apiKey; 
+// var urlPlaceDetails = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' +placeId + '&key=' + apiKey; 
 
 // StoreGoogle.find({}).exec()
 // .then(function(storeObjs){
@@ -263,33 +157,6 @@ var requestP = Promise.promisify(request)
 // }
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// //Place Details Request: 
-// 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJN1t_tDeuEmsRUsoyG83frY4&key=AddYourOwnKeyHere'
-
-
-
-
-
-/*
-API LIMITS 
-
-Limit is 150,000 requests per 24 hour period with verification of identity: credit card 
-
-
-Note that the Text Search service is subject to a 10-times multiplier. That is, each Text Search request that you make will count as 10 requests against your quota.
-
-So the first time through, if we use the text search 
-it'll take us 17 days to get the data ---- TOO LONG
-
-
-so we'll have to use nearby search --- two days to get the placeids
-and another two days to get the place details
-
-
-
-*/
 
 
 
@@ -297,56 +164,3 @@ and another two days to get the place details
 
 
 
-
-
-
-
-
-
-
-
-
-// Thing.find({}).remove(function() {
-//   Thing.create({
-//     name : 'Development Tools',
-//     info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-//   }, {
-//     name : 'Server and Client integration',
-//     info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-//   }, {
-//     name : 'Smart Build System',
-//     info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-//   },  {
-//     name : 'Modular Structure',
-//     info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-//   },  {
-//     name : 'Optimized Build',
-//     info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-//   },{
-//     name : 'Deployment Ready',
-//     info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
-//   });
-// });
-
-  // User.find({}).remove(function() { 
-  //   console.log('beginning to populate')  
-  //   User.create({
-  //     provider: 'local',
-  //     name: 'Test User',
-  //     email: 'test@test.com',
-  //     password: 'test', 
-  //     firstName: 'testy', 
-  //     lastName: 'testy'
-  //   }, {
-  //     provider: 'local',
-  //     role: 'admin',
-  //     name: 'Admin',
-  //     email: 'admin@admin.com',
-  //     password: 'admin', 
-  //     firstName: 'adminy', 
-  //     lastName: 'adminy'
-  //   }, function() {
-  //       console.log('finished populating users');
-  //     }
-  //   );
-  // });
