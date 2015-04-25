4a2714ccad29ef8265af4a026e0f8dc331eacaf7
Jan Bialostok <janbialostok@gmail.com>
2015-04-25 12:29:04 -0400

    Last two google places api calls finished

--- server/config/seed.js
+++ server/config/seed.js
@@ -89,7 +89,7 @@ StoreGoogle.find({}).remove().exec()
 	arrayOfStores = arrayOfStores.slice(1)		
 	// change this line to slice the part of the subarray you're responsible for 
 	// note that the last index is not inclusive 
-	arrayOfStores = arrayOfStores.slice(80001, 95002)
+	arrayOfStores = arrayOfStores.slice(125004, 140005)
 	return makeApiCallToPlaceSearch(0, arrayOfStores)
 })
 .then(null, function(err){
@@ -103,7 +103,7 @@ if (index === array.length-1){
 	// when we're at the end of our subarray, we export the collection
 	// export the transformed objects so we can double check seriality
 	// change the name of the file to appropriate designate the range of objects retrieved before uncommenting this out 
-  exec("mongoexport --db snapmap-dev --collection storegoogles --out storegoogles_80001_95002.json"); 
+  exec("mongoexport --db snapmap-dev --collection storegoogles --out storegoogles_125004_140005.json"); 
   return;
 }
 
