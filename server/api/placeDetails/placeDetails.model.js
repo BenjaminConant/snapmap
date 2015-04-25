a48355b45cc59c57ab999ccb3700957b4b628b85
Ayana <ayana.d.i.wilson@gmail.com>
2015-04-25 12:26:20 -0400

    config for place details request

---
+++ server/api/placeDetails/placeDetails.model.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Promise = require('bluebird');
+
+var PlaceDetailsSchema = new Schema({
+  address_components : [
+         {
+            long_name: String,
+            short_name: String,
+            types: [String]
+         }
+      ],
+  formatted_address: String,
+  formatted_phone_number: String,
+      geometry: {
+         location: {
+           lat: Number,
+           lng: Number
+         }
+      },
+  icon: String,
+  id: String,
+  international_phone_number: String,
+  name: String,
+  opening_hours: {
+         open_now: Boolean,
+         periods: [
+            {
+               close: {
+                  day: Number,
+                  time: String
+               },
+               open: {
+                  day: Number,
+                  time: String
+               }
+            }  
+         ],
+         weekday_text: [String]
+       },
+
+  place_id: String,
+  scope: String,
+  alt_ids: [
+         {
+            place_id: String,
+            scope: String
+         }
+      ],
+  rating: Number,
+  reference: String,
+  reviews: [
+         {
+            aspects: [
+               {
+                  rating: Number,
+                  type: String
+               }
+            ],
+            author_name: String,
+            author_url: String,
+            language: String,
+            rating: Number,
+            text: String,
+            time: Number
+         } 
+      ],
+  types: [String],
+  url: String,
+  vicinity: String,
+  website: String
+}, {strict: false});    // this enables us to assign key-value pairs that are defined by this schema to store documents
+
+
+PlaceDetailsSchema.index({ location: "2d" })
+
+PlaceDetailsSchema.set('toJSON', {
+  virtuals: true
+});
+
+module.exports = mongoose.model('PlaceDetails', PlaceDetailsSchema);
--- server/config/seed.js
+++ server/config/seed.js
@@ -8,6 +8,8 @@
 var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
 var Store = require('../api/store/store.model');
+var PlaceDetails = require('../api/placeDetails/placeDetails.model');
+
 var fs = require('fs');
 var exec = require('child_process').exec; 
 var path = require('path'); 
@@ -28,9 +30,9 @@ var LineReader = Promise.promisify(require('node-line-reader').LineReader);
 //   return "mongoexport --db snapmap-dev --collection " + collection + " --out " + path.join(__dirname,"/db_data") + "/" + collection + ".json"
 // }
 
-// exec("mongoimport --db snapmap-dev --collection storegoogles storegoogles_80001_95002.json --jsonArray"); 
+// exec("mongoimport --db snapmap-dev --collection storegoogles storegoogles_195000_210000.json --jsonArray"); 
 
-// exec("mongoexport --db snapmap-dev --collection storegoogles --out storegoogles_40443_43003.json"); 
+// exec("mongoexport --db snapmap-dev --collection storegoogles --out storegoogles_first_half.json --jsonArray"); 
 
 //exports the store collection into a json file 
 // exec("mongoexport --db snapmap-dev --collection stores --out stores.json")
@@ -150,33 +152,52 @@ var LineReader = Promise.promisify(require('node-line-reader').LineReader);
 
 // var urlPlaceDetails = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' +placeId + '&key=' + apiKey; 
 
-// StoreGoogle.find({}).exec()
-// .then(function(storeObjs){
-//   console.log('in then', storeObjs[0])
-//   storeObjs = storeObjs.slice(0, 4)
-//   storeObjs.forEach(makeApiCallToPlaceDetails)
-// })
 
-// function makeApiCallToPlaceDetails(place){
+PlaceDetails.find({}).remove().exec()
+.then(function(){
+	return readFile(__dirname + '/../../storegoogles_first_half.json')
+})
+.then(function(storeObjs){
+	storeObjs = JSON.parse(storeObjs)
+	console.log('in then', storeObjs[0])
+	storeObjs = storeObjs.slice(0, 15000)
+	return makeApiCallToPlaceDetails(0, storeObjs)
+})
+.then(null, function(err){
+	console.log('err: ', err)
+})
 
-//   var urlPlaceDetails = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' +place_id + '&key=AIzaSyDl2B0kbv7OI2PqqvwX6vMboXK9d333G_k';
-//   var body; 
-//   requestP(urlPlaceDetails).then(function(response){
-//     console.log('body: ', response[0].body)
-//     // choose the first object in the array -- big gamble, should add err handling here as well as some checking that this is the right place 
-//       body = JSON.parse(response[0].body)
-      // console.log('parsed response: ', body)
-      //store entire response object -- create schema that matches this structure; need to create StoreDetails model*/
-//       StoreDetails.create(body.results[0])
-//       .then(function(createdGoogleStore){
-//         console.log('store created: ', createdGoogleStore)
-//       })
-//       .then(null, function(err){
-//         console.log('err: ', err)
-//       })
-//     // })
-//   }, function(err){
-//     console.log('err: ', err)
-//   }) 
-// }
+function makeApiCallToPlaceDetails(index, array){
+
+	if(index === array.length-1){
+		return;
+	}
+
+	var store = array[index];
+	console.log('store: ', store)
+
+	if (store && store.place_id){
+	 
+	  var urlPlaceDetails = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' +store.place_id + '&key=AIzaSyAT__B68i6m_QFqf8pf2kCKGy5s2o20iG4';
+	  var body; 
+	  requestP(urlPlaceDetails)
+	  .then(function(response){
+	    console.log('body!!!!!: ', response[0].body)
+	    // choose the first object in the array -- big gamble, should add err handling here as well as some checking that this is the right place 
+	    body = JSON.parse(response[0].body)
+	    console.log('parsed response: ', body)
+	    // store entire response object -- create schema that matches this structure; need to create StoreDetails model*/
+	    return PlaceDetails.create(body.result)
+	  })
+		.then(function(createdPlaceDetails){
+	    console.log('store created: ', createdPlaceDetails)
+	    return makeApiCallToPlaceDetails(index + 1, array)
+	  })
+	}
+
+	else {
+		return makeApiCallToPlaceDetails(index + 1, array);
+	} 
+
+}
 
